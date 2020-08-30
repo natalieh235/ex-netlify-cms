50bfefe462c291e05f21de2213dc247882240dc8
Natalie <natalie.huang99@gmail.com>
2020-08-29 17:58:59 -0700

    about

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,31 @@
 import Head from 'next/head'
 import { Component } from 'react'
-//import { attributes, react as Content} from '../content/about.md';
+import { attributes, react as Content} from '../content/about.md';
 
 export default class About extends Component {
     render() {
-        return(
-            <div></div>
+
+        
+        let { title, frames } = attributes
+        return (
+            <>
+                <Head>
+                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+                </Head>
+                <article>
+                    <h1>{title}</h1>
+
+                    {frames.map((frame) => (
+                        <>
+                            <p>{frame.subtitle}</p>
+                            <h2>{frame.header}</h2>
+                            <p>{frame.description}</p>
+                            <button>{frame.button}</button>
+                            <br />
+                        </>
+                    ))}
+                </article>
+            </>
         )
-
-        // let { title, frames } = attributes
-        // return (
-        //     <>
-        //         <Head>
-        //             <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        //         </Head>
-        //         <article>
-        //             <h1>{title}</h1>
-
-        //             {frames.map((frame) => (
-        //                 <>
-        //                     <p>{frame.subtitle}</p>
-        //                     <h2>{frame.header}</h2>
-        //                     <p>{frame.description}</p>
-        //                     <button>{frame.button}</button>
-        //                     <br />
-        //                 </>
-        //             ))}
-        //         </article>
-        //     </>
-        // )
     }
 }
